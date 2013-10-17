b7ec4e9af41de7863de9d3c648c64b5103a2cebb
marcosribeiro <ribeiro.marcos17@gmail.com>
2013-10-17 15:53:39 -0300

    Incluido Speaker e Contact no Admin (ainda com erro)

--- eventex/core/tests/test_models.py
+++ eventex/core/tests/test_models.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from django.test import TestCase
-from eventex.core.models import Speaker
+from eventex.core.models import Speaker, Contact
+from django.core.exceptions import ValidationError
 
 
 class SpeakerModelTest(TestCase):
@@ -18,3 +19,43 @@ class SpeakerModelTest(TestCase):
     def test_unicode(self):
         'unicode de Speaker deve ser o nome'
         self.assertEqual(u'Marcos Ribeiro', unicode(self.speaker))
+
+
+class ContactModelTest(TestCase):
+    def setUp(self):
+        self.speaker = Speaker.objects.create(name='Marcos Ribeiro',
+                                              slug='marcos-ribeiro',
+                                              url='http://marcosribeiro.com',
+                                              description='Developer')
+
+    def test_email(self):
+        contact = Contact.objects.create(speaker = self.speaker,
+                                         kind = 'E',
+                                         value = 'marcos@ribeiro.com')
+        self.assertEqual(1, contact.pk)
+
+    def test_phone(self):
+        contact = Contact.objects.create(speaker = self.speaker,
+                                         kind = 'P',
+                                         value = '21-2345678')
+        self.assertEqual(1, contact.pk)
+
+    def test_fax(self):
+        contact = Contact.objects.create(speaker = self.speaker,
+                                         kind = 'F',
+                                         value = '21-2345678')
+        self.assertEqual(1, contact.pk)
+
+    def test_kind(self):
+        'Tipo de contato deve ser E, P ou F'
+        contact = Contact.objects.create(speaker = self.speaker,
+                                         kind = 'A',
+                                         value = 'B')
+        self.assertRaises(ValidationError, contact.full_clean)
+
+    def test_unicode(self):
+        'A representação de um contato deve ser o valor'
+        contact = Contact.objects.create(speaker = self.speaker,
+                                         kind = 'E',
+                                         value = 'marcos@ribeiro.com')
+        self.assertEqual(u'marcos@ribeiro.com', unicode(contact))
